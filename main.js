705ac05fac86786a5c1ac0dcd4631214459cdfd3
skippy <rcthedev@gmail.com>
2020-01-13 02:39:22 -0700

    redoing weather app with js

---
+++ main.js
@@ -0,0 +1,28 @@
+const api = {
+  key: "b4ea2bed4153c798555b4bc2134b6d44",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode ==13) {
+    getResults(searchbox.value);
+    console.log(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch('${api.base}weather?q=${query}&units=imperial&APPID=${api.key}')
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+
+}
+
+
+
+function displayResults (weather) {
+  console.log(weather);
+}
